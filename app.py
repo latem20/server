f428a09ffd5f012a89b7ea5573585d4a58efba1e
latem20 <59595246+latem20@users.noreply.github.com>
2020-01-13 10:31:55 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,8 +23,7 @@ class myHandler(BaseHTTPRequestHandler):
 	
 	#Handler for the GET requests
 	def do_GET(self):
-		
-		print(self.path.split('/')[-1]))
+		print(self.path.split('/')[-1])
 		nombre=self.path.split('/')[-1])
 		print('123dxrcfvgh12321')
 		datos=''
