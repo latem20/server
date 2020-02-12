8ab32b604819df36f9a41be1e27cb024813459bf
latem20 <59595246+latem20@users.noreply.github.com>
2020-02-11 23:32:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import mqtt
 
 try:
 	from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
@@ -15,7 +16,7 @@ import os
 port = int(os.environ.get("PORT", 5000))	
 PORT_NUMBER = port
 
-import mqtt
+
 
 #This class will handles any incoming request from
 #the browser 
